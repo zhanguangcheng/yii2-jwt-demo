66f28aefa45318e986b5cb4720a1268cba849f5a
zgc <14712905@qq.com>
2022-09-28 23:33:45 +0800

    update

---
+++ controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * 登录
+     */
+    public function actionLogin()
+    {
+        
+    }
+
+    /**
+     * 退出
+     */
+    public function actionLogout()
+    {
+
+    }
+
+    /**
+     * 刷新access-token
+     */
+    public function actionRefreshToken()
+    {
+
+    }
+
+    /**
+     * 获取所有会话
+     */
+    public function actionSessions()
+    {
+
+    }
+}
